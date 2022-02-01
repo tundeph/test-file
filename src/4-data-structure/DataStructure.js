b4342d417a04fced725d93a1376ba2365d8601bb
tundeph <54335828+tundeph@users.noreply.github.com>
2022-02-01 17:05:42 +0100

    Create DataStructure.js

---
+++ src/4-data-structure/DataStructure.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const DataStructure = () => {
+  //chunk algorithm
+  // const chunk = (arr, size) => {
+  //   const newArr = []
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (!newArr[newArr.length - 1] || newArr[newArr.length - 1].length === size) {
+  //       newArr.push([arr[i]])
+  //     } else {
+  //       newArr[newArr.length - 1].push(arr[i])
+  //     }
+  //   }
+
+  //   return newArr
+  // }
+
+  // const arr = [3, 5, 1, 2, 9, 3, 2, 6, 7]
+  // const siz = 2
+  // console.log(chunk(arr, siz))
+
+  return <div></div>
+}
+
+export default DataStructure
