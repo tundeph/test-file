3c5f528c66ff103f8183ba6ea468a72d6690f428
tundeph <54335828+tundeph@users.noreply.github.com>
2022-02-01 17:05:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react"
 import "./App.css"
 import "./index.css"
-import ReactInfiniteScroll from "./1-react-infinite-scroll"
+//import ReactInfiniteScroll from "./1-react-infinite-scroll"
 // import HamburgerMenu from "./3-hamburger-menu/HamburgerMenu"
 // import TailwindPlayground from "./2-tailwind-playground"
+import DataStructure from "./4-data-structure/DataStructure"
 
 function App() {
   return (
     <>
       {/* <TailwindPlayground /> */}
-      <ReactInfiniteScroll />
+      {/* <ReactInfiniteScroll /> */}
       {/* <HamburgerMenu /> */}
+      <DataStructure />
     </>
   )
 }
