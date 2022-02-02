5b5a08cca2f5097ab0eeb7b9016e8105619e1026
tundeph <54335828+tundeph@users.noreply.github.com>
2022-02-02 22:21:01 +0100

    Update DataStructure.js

--- src/4-data-structure/DataStructure.js
+++ src/4-data-structure/DataStructure.js
@@ -1,7 +1,8 @@
 import React from "react"
 
 const DataStructure = () => {
-  //chunk algorithm
+  //-------------------------
+  //-----chunk algorithm
   // const chunk = (arr, size) => {
   //   const newArr = []
   //   for (let i = 0; i < arr.length; i++) {
@@ -18,6 +19,29 @@ const DataStructure = () => {
   // const arr = [3, 5, 1, 2, 9, 3, 2, 6, 7]
   // const siz = 2
   // console.log(chunk(arr, siz))
+
+  //--------------------------------
+  //----to sort by the number of occurrenc of an element in an array
+  //['Foo', 'Bar', 'Foo', 'Bar', 'Bar', 'Faz', 'Fee']
+
+  // const sorter =(arrays)=>{
+
+  // 	const res = {}
+
+  // 	arrays.forEach((array)=>{
+  //   	if(!res[array]){
+  //     	res[array] = 1
+  //     }else{
+  //     res[array] += 1
+  //     }
+  //   })
+
+  // const finalArray = Object.keys(res)
+  // finalArray.sort((a,b)=>{
+  // 	res[a] === res[b] ? 0 : res[a] > res[b] ? -1 : 1
+  // })
+  // return finalArray
+  // }
 
   return <div></div>
 }
