ca9d39936d28162302524fd58e72d18bb6d7cd7b
tundeph <54335828+tundeph@users.noreply.github.com>
2022-03-19 14:35:57 +0000

    Update DataStructure.js

--- src/4-data-structure/DataStructure.js
+++ src/4-data-structure/DataStructure.js
@@ -1,50 +1,54 @@
 import React from "react"
 
 const DataStructure = () => {
+  //------------------------
+  //string reversal algorithm
+
+  const stringReversal = (str) => {
+    return str.split("").reverse().join("")
+  }
+
+  console.log(stringReversal("Tunde"))
+
   //-------------------------
   //-----chunk algorithm
-  const chunk = (arr, size) => {
-    const newArr = []
-    for (let i = 0; i < arr.length; i++) {
-      if (
-        !newArr[newArr.length - 1] ||
-        newArr[newArr.length - 1].length === size
-      ) {
-        newArr.push([arr[i]])
-      } else {
-        newArr[newArr.length - 1].push(arr[i])
-      }
-    }
+  // const chunk = (arr, size) => {
+  //   const newArr = []
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (
+  //       !newArr[newArr.length - 1] ||
+  //       newArr[newArr.length - 1].length === size
+  //     ) {
+  //       newArr.push([arr[i]])
+  //     } else {
+  //       newArr[newArr.length - 1].push(arr[i])
+  //     }
+  //   }
 
-    return newArr
-  }
+  //   return newArr
+  // }
 
   // const arr = [3, 5, 1, 2, 9, 3, 2, 6, 7]
   // const siz = 2
   // console.log(chunk(arr, siz))
 
   //--------------------------------
-  //----to sort by the number of occurrenc of an element in an array
+  //----to sort by the number of occurrence of an element in an array
   //['Foo', 'Bar', 'Foo', 'Bar', 'Bar', 'Faz', 'Fee']
 
-  // const sorter =(arrays)=>{
+  // const array = ["Foo", "Bar", "Foo", "Bar", "Bar", "Faz", "Fee"]
+  // const sorter = (array) => {
+  //   const obj = array.reduce((acc, item) => {
+  //     acc[item] ? (acc[item] += 1) : (acc[item] = 1)
+  //     return acc
+  //   }, {})
 
-  // 	const res = {}
-
-  // 	arrays.forEach((array)=>{
-  //   	if(!res[array]){
-  //     	res[array] = 1
-  //     }else{
-  //     res[array] += 1
-  //     }
+  //   return Object.entries(obj).sort((a, b) => {
+  //     return a[1] > b[1] ? 1 : a[1] < b[1] ? -1 : 0
   //   })
-
-  // const finalArray = Object.keys(res)
-  // finalArray.sort((a,b)=>{
-  // 	res[a] === res[b] ? 0 : res[a] > res[b] ? -1 : 1
-  // })
-  // return finalArray
   // }
+
+  // console.log(sorter(array))
 
   return <div></div>
 }
