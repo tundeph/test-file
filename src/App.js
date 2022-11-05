5355deac2164082c7a4845f74de79d29275a6414
tundeph <54335828+tundeph@users.noreply.github.com>
2022-11-05 11:20:05 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import "./index.css"
 //import ReactInfiniteScroll from "./1-react-infinite-scroll"
 // import HamburgerMenu from "./3-hamburger-menu/HamburgerMenu"
 // import TailwindPlayground from "./2-tailwind-playground"
-import DataStructure from "./4-data-structure/DataStructure"
+// import DataStructure from "./4-data-structure/DataStructure"
+// import Rebilly from "./5-rebilly"
+import ReduxToolkit from "./6-redux-toolkit"
 
 function App() {
-  return (
-    <>
-      {/* <TailwindPlayground /> */}
-      {/* <ReactInfiniteScroll /> */}
-      {/* <HamburgerMenu /> */}
-      <DataStructure />
-    </>
-  )
+	return (
+		<>
+			{/* <TailwindPlayground /> */}
+			{/* <ReactInfiniteScroll /> */}
+			{/* <HamburgerMenu /> */}
+			{/* <DataStructure /> */}
+			{/* <Rebilly /> */}
+			<ReduxToolkit />
+		</>
+	)
 }
 
 export default App
